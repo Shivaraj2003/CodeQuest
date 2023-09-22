96c5493182e57e8254bfd0dd5c766fa78c38b230
Shivaraj2003 <shivarajshetty635@gmail.com>
2023-09-22 18:58:54 +0530

    Added  file for AP Generator

---
+++ Day18/AP/Solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static int[] termsOfAP(int x) {
+        int[] ans = new int[x];
+        int j = 0;
+
+        for (int i = 0; j < x; i++) {
+            int temp = 3 * (i+1) + 2;
+            if (temp % 4 != 0) {
+                ans[j++] = temp;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter total number of terms");
+        int x = sc.nextInt();
+        int[] result = termsOfAP(x);
+        System.out.println("First " + x + " terms of the arithmetic progression:");
+        for (int term : result) {
+            System.out.print(term + " ");
+        }
+    }
+}
